a58122127c2183be72d92b3c9eb6303940adf971
Bogdan <Bogdan_070509@mail.ru>
2025-02-10 20:47:28 +0300

    Ready project

--- first.py
+++ first.py
@@ -2,11 +2,49 @@ def hello():
     return int(input("Введите размер поля: "))
 
 def pole(a):
+    map = []
     for i in range(a):
-        print(a * " _ ")
+        map.append(['_'] * a)
+    return map
 
-def mainn():
-    side = hello()
-    pole(side)
+def show(map):
+    for row in map:
+        print(" ".join(row))
 
-mainn()
+def move(x, y, map, side):
+    map[y][x] = "_"
+    if side == "left":
+        x -= 1
+        if x < 0:
+            x = len(map) - 1
+
+    elif side == "right":
+        x += 1
+        if x > len(map) - 1:
+            x = 0
+
+    elif side == "up":
+        y -= 1
+        if y < 0:
+            y = len(map) - 1
+
+    elif side == "down":
+        y += 1
+        if y > len(map) - 1:
+            y = 0
+
+    map[y][x] = "x"
+    return x, y, map
+
+
+def main():
+    size = hello()
+    map = pole(size)
+    x, y = 0, 0
+    map[y][x] = "x"
+    while True:
+        show(map)
+        side = input("left, up, right, down")
+        x, y, map = move(x, y, map, side)
+
+main()
