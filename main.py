3a5e1c80d88fd397da1c1e023d55a1f75fe8c658
Bogdan <Bogdan_070509@mail.ru>
2025-02-22 17:22:46 +0300

    Result

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import game
+import move
 from random import randint
 
 def hello():
@@ -6,26 +6,30 @@ def hello():
 
 def pole(a):
     map = []
-    for i in range(a):
-        map.append(['_'] * a)
+    for _ in range(a):
+        row = []
+        for _ in range(a):
+            if randint(1, 3) == 1:
+                row.append('#')
+            else:
+                row.append('_')
+        map.append(row)
     return map
 
 def show(map):
     for row in map:
         print(" ".join(row))
 
-
 def main():
     size = hello()
     map = pole(size)
     x, y = 0, 0
     map[y][x] = "x"
-    a, b = randint(1, size), randint(1, size)
-    map[b][a] = "0"
 
     while True:
         show(map)
         side = input("left, up, right, down")
-        x, y, map = game.move(x, y, map, side)
+        x, y, map = move.game(x, y, map, side)
+
 
 main()
--- move.py
+++ move.py
@@ -1,24 +1,26 @@
-def move(x, y, map, side):
-    map[y][x] = "_"
+def game(x, y, map, side):
+    x_old, y_old = x, y
     if side == "left":
         x -= 1
         if x < 0:
             x = len(map) - 1
-
     elif side == "right":
         x += 1
         if x > len(map) - 1:
             x = 0
-
     elif side == "up":
         y -= 1
         if y < 0:
             y = len(map) - 1
-
     elif side == "down":
         y += 1
         if y > len(map) - 1:
             y = 0
 
+    if map[y][x] == '#':
+        print("Вы не можете этого сделать")
+        return x_old, y_old, map
+    map[x_old][y_old] = "_"
     map[y][x] = "x"
+
     return x, y, map
